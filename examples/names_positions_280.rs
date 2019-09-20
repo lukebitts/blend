b6953eef0a8678fe651a5b76fff7b2a847fb0e38
lukebitts <lbittencs@gmail.com>
2019-09-20 16:49:05 -0300

    fixed errors on 'names_positions_280' example

--- examples/names_positions_280.rs
+++ examples/names_positions_280.rs
@@ -1,19 +1,17 @@
-use blend::{Blend, Instance};
-use std::{env, io, path};
+use blend::Blend;
+use std::{env, path};
 
 fn main() {
     let base_path = path::PathBuf::from(
         env::var_os("CARGO_MANIFEST_DIR").expect("could not find cargo manifest dir"),
     );
     let blend_path = base_path.join("examples/blend_files/2_80.blend");
-    let blend = Blend::from_path("examples/blend_files/2_80.blend");
+    let blend = Blend::from_path(blend_path);
 
     for obj in blend.get_by_code(*b"OB") {
         let loc = obj.get_f32_vec("loc");
         let name = obj.get("id").get_string("name");
 
-        //println!("\"{}\" at {:?}", name, loc);
-        println!("{}", obj);
-        break
+        println!("\"{}\" at {:?}", name, loc);
     }
 }
